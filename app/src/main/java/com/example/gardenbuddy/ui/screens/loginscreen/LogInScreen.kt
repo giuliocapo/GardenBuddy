00b3ef8f310c2018423fa53cc78b8d2235a006da
slash <lorenzo.tanzi997@gmail.com>
2025-01-18 18:01:31 +0100

    - loginScreen UI modified

--- app/src/main/java/com/example/gardenbuddy/ui/screens/loginscreen/LogInScreen.kt
+++ app/src/main/java/com/example/gardenbuddy/ui/screens/loginscreen/LogInScreen.kt
@@ -1,12 +1,20 @@
 package com.example.gardenbuddy.ui.screens.loginscreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -39,7 +47,7 @@ fun LogInScreen(
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
-    Column() {
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
         Text(text = "Log In")
 
         OutlinedTextField(
@@ -62,11 +70,23 @@ fun LogInScreen(
             Text(text = "Error: $errorMessage")
         }
 
+        Spacer(modifier = Modifier.height(16.dp))
+
         Button(
             onClick = { logInViewModel.logIn(email, password) },
             enabled = !isLoading
         ) {
             Text(if (isLoading) "Loading..." else "Log In")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically){
+            HorizontalDivider(Modifier.width(50.dp))
+            Box(Modifier.padding(10.dp, 0.dp)){
+                Text("or")
+            }
+            HorizontalDivider(Modifier.width(50.dp))
         }
 
         Spacer(modifier = Modifier.height(16.dp))
